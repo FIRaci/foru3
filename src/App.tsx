0638ece3cd0d9c222852278b436657f5bc67813c
FIRaci <bequocthanh2882005@gmail.com>
2026-07-27 21:16:41 +0700

    Fix build and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import gsap from 'gsap';
 import confetti from 'canvas-confetti';
 import { Gift, Heart, Sparkles } from 'lucide-react';
